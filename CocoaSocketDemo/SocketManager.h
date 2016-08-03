f691eeb56d39738b3172edb2593812926838e685
卢强 <qiang.lu@renren-inc.com>
2016-08-03 16:39:34 +0800

    first commit

---
+++ CocoaSocketDemo/SocketManager.h
@@ -0,0 +1,13 @@
+//
+//  SocketManager.h
+//  CocoaSocketDemo
+//
+//  Created by lasso on 16/8/3.
+//  Copyright © 2016年 lasso. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SocketManager : NSObject
+
+@end
---
+++ CocoaSocketDemo/SocketManager.m
@@ -0,0 +1,13 @@
+//
+//  SocketManager.m
+//  CocoaSocketDemo
+//
+//  Created by lasso on 16/8/3.
+//  Copyright © 2016年 lasso. All rights reserved.
+//
+
+#import "SocketManager.h"
+
+@implementation SocketManager
+
+@end
