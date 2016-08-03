2289944f6be1036a93dda7efa486c05a231c35be
卢强 <qiang.lu@renren-inc.com>
2016-08-03 10:52:51 +0800

    Initial Commit

---
+++ CocoaSocketDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CocoaSocketDemo
+//
+//  Created by lasso on 16/8/3.
+//  Copyright © 2016年 lasso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CocoaSocketDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CocoaSocketDemo
+//
+//  Created by lasso on 16/8/3.
+//  Copyright © 2016年 lasso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
